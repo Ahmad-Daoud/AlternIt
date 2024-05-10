ae4594b5351d21e04246f07218c988b5d58fca63
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2024-05-10 21:13:04 +0200

    Ajout d'id pour les contacts et evenements + page d'ajout d'évenement avec possibilité de joindre des contacts à un evenement

--- app/src/main/java/com/example/alternit/MainActivity.kt
+++ app/src/main/java/com/example/alternit/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.alternit
+import com.example.alternit.ui.theme.AlternItTheme
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,9 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import android.util.Log
-import com.example.alternit.R
-import com.example.alternit.ui.theme.AlternItTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.*
@@ -21,13 +19,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import android.content.Context
-import org.json.JSONArray
-import org.json.JSONObject
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.Alignment
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 class MainActivity : ComponentActivity() {
@@ -35,9 +38,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AlternItTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MyApp(context = applicationContext) // Pass the context here
+                    MyApp(context = applicationContext)
                 }
             }
         }
@@ -56,24 +58,36 @@ fun ContactList(contacts: List<Contact>) {
     }
 }
 
+@Composable
+fun EventList(events: List<Event>) {
+    LazyColumn {
+        items(events) { event ->
+            Text(
+                text = "${event.eventName} - ${event.eventDate}",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MyApp(context: Context) {
     var showDialog by remember { mutableStateOf(false) }
     val contactsState = remember { mutableStateOf(emptyList<Contact>()) }
+    val eventsState = remember { mutableStateOf(emptyList<Event>()) }
 
-    // Read contacts when MyApp is recomposed
     LaunchedEffect(Unit) {
         refreshContactList(context, contactsState)
+        refreshEventList(context, eventsState)
     }
 
     Scaffold(
         bottomBar = {
             BottomAppBar(
                 modifier = Modifier.fillMaxWidth(),
-                content = {
-                    // Empty content, as we're not adding anything to the bottom app bar
-                }
+                content = { }
             )
         },
         floatingActionButton = {
@@ -89,13 +103,17 @@ fun MyApp(context: Context) {
         },
         floatingActionButtonPosition = FabPosition.Center
     ) {
-        ContactList(contacts = contactsState.value)
+        Column {
+            ContactList(contacts = contactsState.value)
+            EventList(events = eventsState.value)
+        }
 
         if (showDialog) {
             AddItemDialog(
                 onClose = { showDialog = false },
                 context = context,
-                onContactAdded = { refreshContactList(context, contactsState) } // Refresh the contact list
+                onContactAdded = { refreshContactList(context, contactsState) },
+                onEventAdded = { refreshEventList(context, eventsState) }
             )
         }
     }
@@ -104,29 +122,42 @@ fun MyApp(context: Context) {
 fun refreshContactList(context: Context, contactsState: MutableState<List<Contact>>) {
     contactsState.value = readContactsFromJson(context)
 }
+fun refreshEventList(context: Context, eventsState: MutableState<List<Event>>) {
+    eventsState.value = readEventsFromJson(context)
+}
 
 
-
-// Data class to represent a contact
 data class Contact(
+    val id: Int,
     val firstName: String,
     val lastName: String,
     val phoneNumber: String,
     val company: String
 )
+data class Event(
+    val id: Int,
+    val eventName: String,
+    val eventDate: String,
+    val eventLocation: String,
+    val contactsLinked: List<Int>
+)
 
 
-// Function to read contacts from JSON file
+enum class DialogType {
+    Contact,
+    Event
+}
+
+
 fun readContactsFromJson(context: Context): List<Contact> {
     val contacts = mutableListOf<Contact>()
-    println("start contacts : \n")
     try {
         val jsonString = context.openFileInput("contacts.json").bufferedReader().use { it.readText() }
-        Log.d("JSON_CONTENT", jsonString) // Add this line to print JSON content
         val jsonArray = JSONArray(jsonString)
         for (i in 0 until jsonArray.length()) {
             val jsonObject = jsonArray.getJSONObject(i)
             val contact = Contact(
+                id = jsonObject.getInt("id"),
                 firstName = jsonObject.getString("prenom"),
                 lastName = jsonObject.getString("nom"),
                 phoneNumber = jsonObject.getString("telephone"),
@@ -137,8 +168,74 @@ fun readContactsFromJson(context: Context): List<Contact> {
     } catch (e: Exception) {
         e.printStackTrace()
     }
-    println("End contacts : \n")
     return contacts
+}
+fun readEventsFromJson(context: Context): List<Event> {
+    val events = mutableListOf<Event>()
+    try {
+        val jsonString = context.openFileInput("events.json").bufferedReader().use { it.readText() }
+        val jsonArray = JSONArray(jsonString)
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(i)
+            val event = Event(
+                id = jsonObject.getInt("id"),
+                eventName = jsonObject.getString("eventName"),
+                eventDate = jsonObject.getString("eventDate"),
+                eventLocation = jsonObject.getString("eventLocation"),
+                contactsLinked = jsonArrayToContactsList(jsonObject.getJSONArray("contactsLinked"))
+            )
+            events.add(event)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return events
+}
+
+private fun jsonArrayToContactsList(jsonArray: JSONArray): List<Int> {
+    val contactsList = mutableListOf<Int>()
+    for (i in 0 until jsonArray.length()) {
+        contactsList.add(jsonArray.getInt(i))
+    }
+    return contactsList
+}
+
+fun getLastContactId(context: Context): Int {
+    var lastContactId = 0
+    try {
+        val jsonString = context.openFileInput("contacts.json").bufferedReader().use { it.readText() }
+        val jsonArray = JSONArray(jsonString)
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(i)
+            val contactId = jsonObject.getInt("id")
+            if (contactId > lastContactId) {
+                lastContactId = contactId
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return lastContactId
+}
+
+
+
+fun getLastEventId(context: Context): Int {
+    var lastEventId = 0
+    try {
+        val jsonString = context.openFileInput("events.json").bufferedReader().use { it.readText() }
+        val jsonArray = JSONArray(jsonString)
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(i)
+            val eventId = jsonObject.getInt("id")
+            if (eventId > lastEventId) {
+                lastEventId = eventId
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return lastEventId
 }
 
 fun writeContactToJson(context: Context, contactName: String, lastName: String, phoneNumber: String, company: String) {
@@ -150,32 +247,59 @@ fun writeContactToJson(context: Context, contactName: String, lastName: String, 
     }
 
     try {
-        // Read existing contacts
         val existingContacts = readContactsFromJson(context).toMutableList()
+        val contactId = getLastContactId(context) + 1
+        existingContacts.add(Contact(contactId ,contactName, lastName, phoneNumber, company))
 
-        // Add the new contact
-        existingContacts.add(Contact(contactName, lastName, phoneNumber, company))
-
-        // Convert the list to JSON array
         val jsonArray = JSONArray(existingContacts.map { contact ->
             JSONObject().apply {
+                put("id", contact.id)
                 put("prenom", contact.firstName)
                 put("nom", contact.lastName)
                 put("telephone", contact.phoneNumber)
                 put("entreprise", contact.company)
             }
         })
-
-        // Write the JSON array to file
         val jsonString = jsonArray.toString()
         val fileOutputStream = context.openFileOutput("contacts.json", Context.MODE_PRIVATE)
         fileOutputStream.write(jsonString.toByteArray())
         fileOutputStream.close()
     } catch (e: Exception) {
         e.printStackTrace()
+        println("Erreur ecriture de contact!")
     }
 }
 
+fun writeEventToJson(context: Context, eventName: String, eventDate: String, eventLocation: String, contactsLinked: List<Int>) {
+    val eventObject = JSONObject().apply {
+        put("eventName", eventName)
+        put("eventDate", eventDate)
+        put("eventLocation", eventLocation)
+        put("contactsLinked", JSONArray(contactsLinked))
+    }
+
+    try {
+        val existingEvents = readEventsFromJson(context).toMutableList()
+        existingEvents.add(Event(getLastEventId(context) + 1, eventName, eventDate, eventLocation, contactsLinked))
+
+        val jsonArray = JSONArray(existingEvents.map { event ->
+            JSONObject().apply {
+                put("id", event.id)
+                put("eventName", event.eventName)
+                put("eventDate", event.eventDate)
+                put("eventLocation", event.eventLocation)
+                put("contactsLinked", JSONArray(event.contactsLinked))
+            }
+        })
+
+        val jsonString = jsonArray.toString()
+        val fileOutputStream = context.openFileOutput("events.json", Context.MODE_PRIVATE)
+        fileOutputStream.write(jsonString.toByteArray())
+        fileOutputStream.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
 
 
 @Composable
@@ -218,10 +342,9 @@ fun AddContactDialog(onClose: () -> Unit, context: Context, onContactAdded: () -
         confirmButton = {
             Button(
                 onClick = {
-                    // Handle contact addition here
                     writeContactToJson(context, contactName, lastName, phoneNumber, company)
-                    onClose()
-                    onContactAdded() // Notify that a contact has been added
+                    onClose() // Close the dialog
+                    onContactAdded() // Trigger refresh of contacts list
                 }
             ) {
                 Text("Ajout Contact")
@@ -237,27 +360,107 @@ fun AddContactDialog(onClose: () -> Unit, context: Context, onContactAdded: () -
     )
 }
 
-fun AddEventDialog(onClose: () -> Unit) {
-    // Functionality for adding event goes here
+@Composable
+fun AddEventDialog(
+    onClose: () -> Unit,
+    context: Context,
+    onEventAdded: () -> Unit
+) {
+    var eventName by remember { mutableStateOf("") }
+    var eventDate by remember { mutableStateOf("") }
+    var eventLocation by remember { mutableStateOf("") }
+    val selectedContacts = remember { mutableStateOf(mutableListOf<Int>()) }
+    val contacts = readContactsFromJson(context) // Retrieve the list of contacts
+    AlertDialog(
+        onDismissRequest = onClose,
+        title = { Text("Ajouter un evenement") },
+        text = {
+            Column {
+                TextField(
+                    value = eventName,
+                    onValueChange = { eventName = it },
+                    label = { Text("Nom") },
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                TextField(
+                    value = eventDate,
+                    onValueChange = { eventDate = it },
+                    label = { Text("Date") },
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                TextField(
+                    value = eventLocation,
+                    onValueChange = { eventLocation = it },
+                    label = { Text("Localisation") }
+                )
+                Text("Choisir des contacts qui font partie de l'evenement:")
+                contacts.forEach { contact ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    ) {
+                        Checkbox(
+                            checked = selectedContacts.value.contains(contact.id),
+                            onCheckedChange = {
+                                if (it) {
+                                    selectedContacts.value.add(contact.id)
+                                } else {
+                                    selectedContacts.value.remove(contact.id)
+                                }
+                            }
+                        )
+                        Text(
+                            text = "${contact.firstName} ${contact.lastName}",
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
+                }
+            }
+        },
+        confirmButton = {
+            Button(
+                onClick = {
+                    writeEventToJson(context, eventName, eventDate, eventLocation, selectedContacts.value)
+                    onClose()
+                    onEventAdded()
+                }
+            ) {
+                Text(
+                    text = "Ajouter l'evenement",
+                    style = TextStyle(fontSize = 11.sp)
+                )
+            }
+        },
+        dismissButton = {
+            Button(
+                onClick = onClose
+            ) {
+                Text(
+                    text = "Annuler",
+                    style = TextStyle(fontSize = 12.sp)
+                )
+            }
+        }
+    )
 }
-
 
 @Composable
 fun AddItemDialog(
     onClose: () -> Unit,
     context: Context,
-    onContactAdded: () -> Unit // Define onContactAdded parameter here
+    onContactAdded: () -> Unit,
+    onEventAdded: () -> Unit
 ) {
     var showDialog by remember { mutableStateOf(false) }
-
+    var dialogType by remember { mutableStateOf(DialogType.Contact) }
     AlertDialog(
         onDismissRequest = onClose,
-        title = { Text("Add Item") },
+        title = { Text("Ajouter un : ") },
         text = { /* No additional text */ },
         confirmButton = {
             Button(
                 onClick = {
-                    // Handle contact button click
+                    dialogType = DialogType.Contact
                     showDialog = true
                 }
             ) {
@@ -267,31 +470,37 @@ fun AddItemDialog(
         dismissButton = {
             Button(
                 onClick = {
-                    // Handle event button click
-                    showDialog = false
-                    onClose()
+                    dialogType = DialogType.Event
+                    showDialog = true
                 }
             ) {
-                Text("Event")
+                Text("Evenement")
             }
         }
     )
 
-    if (showDialog) {
-        AddContactDialog(
-            onClose = {
-                showDialog = false
-                onClose()
-            },
-            context = context,
-            onContactAdded = onContactAdded // Pass onContactAdded to AddContactDialog
-        )
-    } else {
-        // AddEventDialog can be called similarly when needed
+    if(showDialog){
+        if (dialogType == DialogType.Contact) {
+            AddContactDialog(
+                onClose = {
+                    showDialog = false
+                    onClose()
+                },
+                context = context,
+                onContactAdded = onContactAdded
+            )
+        } else {
+            AddEventDialog(
+                onClose = {
+                    showDialog = false
+                    onClose()
+                },
+                context = context,
+                onEventAdded = onEventAdded
+            )
+        }
     }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
